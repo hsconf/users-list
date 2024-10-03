81bb3ab27e05c65dd892ea1eb71c6fe967e7313b
Akylbek <hsplay74@gmail.com>
2024-10-04 00:08:34 +0600

    App init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ToolBar from "./components/ToolBar/ToolBar.tsx";
+
+const App = () => (
+    <>
+    <header>
+        <ToolBar />
+    </header>
+    <main className='container'>
+
+    </main>
+    </>
+);
+
+export default App
---
+++ src/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,12 @@
+
+const ToolBar = () => {
+    return (
+        <nav className="navbar bg-primary">
+            <div className="container">
+                <a className="navbar-brand text-light fw-bold" href="#">Users Tool</a>
+            </div>
+        </nav>
+    );
+};
+
+export default ToolBar;
